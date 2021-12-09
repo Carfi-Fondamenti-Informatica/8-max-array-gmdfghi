95702075d0dbd81295b25b8feed04104a101d605
gmdfghi <91668752+gmdfghi@users.noreply.github.com>
2021-12-09 10:23:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,6 @@
 
 #include "lib.h"
-float numeromassimo( const float numeri[], int dim){
+float numeroMassimo( const float numeri[], int dim){
   numeri [dim];
   float massimo=numeri[0];
   for (int i=0; i<dim;i++){
